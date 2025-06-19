b4a433af14af6dc521403ddeef250ede08189802
ajayshsh7 <ajayshsh7@gmail.com>
2025-06-19 18:47:45 +0530

    contact page update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,14 @@
-// app/not-found.tsx
+
 'use client';
 
-import Link from 'next/link';
+import { Component } from 'react';
+import ContactForm from '@/components/ContactForm';
 import styles from './contact.module.css';
 
 export default function NotFound() {
   return (
-    <div className={styles.errorContainer}>
-      <h1 className={styles.errorCode}>404</h1>
-      <p className={styles.errorMessage}>Oops! Page not found.</p>
-      <Link href="/" className={styles.homeBtn}>Go Back Home</Link>
+    <div className={styles.contactContainer}>
+      <ContactForm />
     </div>
   );
 }
