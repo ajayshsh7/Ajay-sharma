44e3a3e0dbc8fe98f43a3ace429e50a0ec53036c
ajayshsh7 <ajayshsh7@gmail.com>
2025-07-03 22:25:17 +0530

    adding sample to photon project

---
+++ app/work/photon/page.tsx
@@ -0,0 +1,11 @@
+import ProjectPage from '@/app/work/ProjectPage';
+
+export default function ProjectOne() {
+  return (
+    <ProjectPage
+      title="Project One"
+      image="/images/photon.png"
+      description="sample template for photon."
+    />
+  );
+}
