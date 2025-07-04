e3bc7449e291109c039b5105a831748555c41334
ajayshsh7 <ajayshsh7@gmail.com>
2025-07-04 18:30:41 +0530

    resuable proeject compoenent changes

--- app/work/ProjectPage.tsx
+++ app/work/ProjectPage.tsx
@@ -1,21 +1,44 @@
 'use client';
-
+import Image, { StaticImageData } from 'next/image';
 import React from 'react';
-import styles from '@/styles/Project.module.css';
+import styles from './projects.module.css';
 
 export interface ProjectProps {
+  imgSrc: StaticImageData;
+  imgAlt?: string;
+  paragraph: string;
   title: string;
-  image: string;
-  description: string;
+  buttonText: string;
+  onButtonClick?: () => void;
 }
 
-const ProjectPage: React.FC<ProjectProps> = ({ title, image, description }) => {
+const ProjectPage: React.FC<ProjectProps> = ({ imgSrc,
+  imgAlt = 'projectImg',
+  paragraph,
+  title,
+  buttonText,
+  onButtonClick, }) => {
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>{title}</h1>
-      <img src={image} alt={title} className={styles.image} />
-      <p className={styles.description}>{description}</p>
+    <div className={styles.homeContainer}>
+      <div className={styles.heroGrid}>
+        <div className={styles.heroImg}>
+          <Image className={styles.myImg} src={imgSrc} alt={imgAlt}/>
+        </div>
+        <div className={styles.heroContent}>
+          <p>{paragraph}</p>
+          <div className={styles.flexContent}>
+            <h1 className={styles.heroText}>{title}</h1>
+            <div className={styles.heroBtns}>
+              <button className={styles.heroBtn} onClick={onButtonClick}>
+                {buttonText}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+            
   );
 };
 
--- app/work/photon/page.tsx
+++ app/work/photon/page.tsx
@@ -1,11 +1,21 @@
+'use client';
+
+
 import ProjectPage from '@/app/work/ProjectPage';
+import aboutheroImg from '../../../public/abouthero.png';
+import { useRouter } from 'next/navigation';
 
 export default function ProjectOne() {
+  const router = useRouter();
   return (
     <ProjectPage
-      title="Project One"
-      image="/images/photon.png"
-      description="sample template for photon."
+      imgSrc={aboutheroImg}
+      imgAlt="Illustration about the developer's work"
+      paragraph="My Work"
+      title="Explore my work on web development and UI/UX design"
+      buttonText="Contact Me"
+      onButtonClick={() => router.push('/contact')}
+
     />
   );
 }
